c3cde3bb5dcc1adc5b458183c20537e6eab6a7b1
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-12-16 17:42:24 +0100

    impostato e collegato il tutto con il database

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        {{-- <ul>
+            @foreach ($movies as $movie)
+                <li>{{ $movie->title }}</li>
+            @endforeach
+        </ul> --}}
+    </div>
+@endsection
